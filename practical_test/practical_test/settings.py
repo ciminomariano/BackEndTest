cb20b2dc75dd0e90421f82c6aa142b2997cf4256
cimin <ciminomariano@gmail.com>
2023-05-19 00:56:02 -0300

    adding db configuration

--- practical_test/practical_test/settings.py
+++ practical_test/practical_test/settings.py
@@ -74,13 +74,17 @@ WSGI_APPLICATION = 'practical_test.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'NAME': 'practicaltest',
+        'USER': 'root',
+        'PASSWORD': 'admin',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
